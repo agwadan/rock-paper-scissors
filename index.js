8f155e8fe3fc6de58ec709b706289aa7a0f5fc2c
agwadan <agwa.dan@gmail.com>
2023-07-17 12:16:52 +0300

    Add all functions

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-//let answer = prompt("Please enter your input to play the game \n Enter 1 for Rock \n Enter 2 for Paper \n Enter 3 for scissors ")
+var playerPoints = 0;
 
+/* Function to get computer's choice */
 const getComputerChoice = () => {
   let values = ['Rock', 'Paper ', 'Scissors'];
   const randomValue = Math.floor(Math.random() * values.length);
-  return console.log(values[randomValue]);
+  return values[randomValue];
 }
 
-getComputerChoice();
+/* Function to get the player's input */
+const getPlayerChoice = () => {
+  return prompt("Enter your selection to play Rock, Paper, Scissors");
+}
+
+const playRound = (playerSelection, computerSelection) => {
+
+  let playerChoice = playerSelection.toLowerCase();
+  let computerChoice = computerSelection.toLowerCase();
+
+  if (playerChoice === computerChoice) {
+    return console.log("It's a tie");
+  }
+
+  if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper') ||
+      (playerChoice === 'paper' && computerChoice === 'rock')
+  ) {
+    playerPoints++;
+    return console.log(`You win : ), ${playerSelection} beats ${computerSelection}`);
+  } else{
+    return console.log(`You lose : ( , ${computerSelection} beats ${playerSelection}`)
+  } 
+}
+
+const game = () => {
+  for (i = 0; i < 5; i++){
+    playRound(getPlayerChoice(), getComputerChoice());
+  }
+  if (playerPoints > 2) {
+    console.log("You've won : )");
+  } else if (playerPoints == 2) {
+    console.log("That was a tie"); 
+  }
+  else {
+    console.log("Sorry . . . you lost the game : (");
+  }
+}
+
+game();
+
